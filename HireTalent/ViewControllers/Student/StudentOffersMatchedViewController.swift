d6eb1a4efd8cce14090f31da745f033a09acfd80
giogurt <giomaster022@gmail.com>
2020-06-08 04:18:37 -0400

    Story completed, students can now rate employers

--- HireTalent/ViewControllers/Student/StudentOffersMatchedViewController.swift
+++ HireTalent/ViewControllers/Student/StudentOffersMatchedViewController.swift
@@ -102,33 +102,6 @@ class StudentOffersMatchedViewController: UITableViewController {
         performSegue(withIdentifier: "viewEmployer", sender: nil)
     }
 
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -136,9 +109,10 @@ class StudentOffersMatchedViewController: UITableViewController {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
         if segue.identifier == "viewEmployer" {
-            var destinationController = segue.destination as! ViewEmployerViewController
+            let destinationController = segue.destination as! ViewEmployerViewController
             
             destinationController.employer = employers[cellSelected]
+            destinationController.employerId = offers[cellSelected].userId
         }
     }
 
--- HireTalent/ViewControllers/Student/ViewEmployerViewController.swift
+++ HireTalent/ViewControllers/Student/ViewEmployerViewController.swift
@@ -10,15 +10,46 @@ import UIKit
 
 class ViewEmployerViewController: UIViewController {
 
+    @IBOutlet weak var employerName: UILabel!
+    @IBOutlet weak var ratingLabel: UILabel!
+    @IBOutlet weak var ratingSlider: UISlider!
+    
     var employer: Employer = Employer()
+    var employerId: String = ""
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
+        startView()
+    }
+    func startView(){
+        let empName = "\(employer.firstName) \(employer.lastName)"
+        self.employerName.text = empName
+        
+        self.ratingLabel.text = "Rating: \(Int(ratingSlider.value))"
     }
     
+    @IBAction func ratingChanged(_ sender: UISlider) {
+        sender.setValue(sender.value.rounded(.down), animated: true)
+        self.ratingLabel.text = "Rating: \(Int(ratingSlider.value))"
+    }
+    
+    @IBAction func rateEmployer(_ sender: UIButton) {
+        employer.rating.append(ratingSlider.value)
+        EmployerDAO.addRating(employerId, employer.rating) { (error) in
+            if error != nil {
+                print("There was a serious problem adding that rating")
+            } else {
+                let alert = UIAlertController(title: "Rating sent!", message: "Thanks for rating your employer, this will help them improve", preferredStyle: .alert)
 
+                alert.addAction(UIAlertAction(title: "Thats cool!", style: .default, handler: nil))
+
+                self.present(alert, animated: true)
+            }
+        }
+    }
+    
     /*
     // MARK: - Navigation
 
